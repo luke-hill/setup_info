6caca4d6a98ae9ac418ca921a850223c565d448c
Luke Hill <lukehill_uk@hotmail.com>
2022-07-05 14:51:41 +0100

    update clipboard scripts

--- refresh_vbox_clipboard.sh
+++ refresh_vbox_clipboard.sh
@@ -1,2 +1,9 @@
-pkill 'VBoxClient --clipboard' -f & sleep 1 && VBoxClient --clipboard
-
+echo "Killing Clipboard"
+pkill 'VBoxClient --clipboard' -f
+sleep 1
+echo "Restarting Clipboard - Copy and Paste Functionality"
+VBoxClient --clipboard
+sleep 1
+echo "Restarting Clipboard - Drag and Drop Functionality"
+VBoxClient --draganddrop
+sleep 1
