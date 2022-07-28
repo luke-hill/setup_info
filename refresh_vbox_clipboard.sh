bf38ecda18562de29c763ad43a0871cb808aca09
Luke Hill <lukehill_uk@hotmail.com>
2022-07-28 09:41:43 +0100

    Make refresh script better

--- refresh_vbox_clipboard.sh
+++ refresh_vbox_clipboard.sh
@@ -1,9 +1,15 @@
 echo "Killing Clipboard"
 pkill 'VBoxClient --clipboard' -f
 sleep 1
+echo "Killing all remaining VBoxClient services"
+killall VBoxClient
+sleep 1
 echo "Restarting Clipboard - Copy and Paste Functionality"
 VBoxClient --clipboard
 sleep 1
 echo "Restarting Clipboard - Drag and Drop Functionality"
 VBoxClient --draganddrop
 sleep 1
+echo "Restarting all remaining VBoxClient services"
+VBoxClient-all
+
