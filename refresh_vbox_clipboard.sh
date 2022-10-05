f3445e00e6d3e50f8bf40de97c72a800e37bdf3c
Luke Hill <lukehill_uk@hotmail.com>
2022-10-05 12:44:12 +0100

    Remove buggy part of script and reduce wait

--- refresh_vbox_clipboard.sh
+++ refresh_vbox_clipboard.sh
@@ -1,15 +1,13 @@
 echo "Killing Clipboard"
 pkill 'VBoxClient --clipboard' -f
-sleep 1
+sleep 0.5
 echo "Killing all remaining VBoxClient services"
 killall VBoxClient
-sleep 1
+sleep 0.5
 echo "Restarting Clipboard - Copy and Paste Functionality"
 VBoxClient --clipboard
-sleep 1
+sleep 0.5
 echo "Restarting Clipboard - Drag and Drop Functionality"
 VBoxClient --draganddrop
-sleep 1
-echo "Restarting all remaining VBoxClient services"
-VBoxClient-all
-
+sleep 0.5
+echo "All Clipboard functionality should now be refreshed"
